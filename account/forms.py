f959bccc913631039dc5e271565a2e107133bc86
czar <czar.yobero@gmail.com>
2021-06-06 09:29:06 -0700

    navigation bar with log out

--- account/forms.py
+++ account/forms.py
@@ -1,8 +1,11 @@
 from django import forms
 from django.contrib.auth.models import User
 
-class UserLoginForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
-    class Meta:
-        model = User
-        fields = ('username', 'password',) 
+class UserLoginForm(forms.Form):
+    username = forms.CharField(max_length=255)
+    password = forms.CharField(max_length=255, widget=forms.PasswordInput)
+
+    widgets = {
+        'username': forms.TextInput(attrs={'class': 'form-control'}),
+        'password': forms.PasswordInput(attrs={'class': 'form-control'})
+    }
--- account/views.py
+++ account/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, HttpResponse
 from .forms import UserLoginForm
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 # Create your views here.
@@ -8,11 +8,24 @@ def user_login(request):
     if request.method == "POST":
         form = UserLoginForm(request.POST)
         if form.is_valid():
-            print("form is valid")
-        else:
-            print("form ain't valid")
-            print(form.errors)
-            print(form.non_field_errors)
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return render(request, 'account/login_success.html')
+                else:
+                    messages.error(request, 'This account is no longer active.')
+                    return render(request, 'account/login.html', {'form': form})
+            else:
+                messages.error(request, 'Invalid login credentials.')
+                return render(request, 'account/login.html', {'form': form})
     else:
         form = UserLoginForm()
     return render(request, 'account/login.html', {'form': form})
+
+
+def user_logout(request):
+    logout(request)
+    return render(request, 'account/logged_out.html')
