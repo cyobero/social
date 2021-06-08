e16c59059e5c799bfe26c3904d3863909b8aa97c
czar <czar.yobero@gmail.com>
2021-06-08 15:49:42 -0700

    user can comment on a blurb

--- comment/admin.py
+++ comment/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.admin.decorators import register
 
 # Register your models here.
+from .models import Comment
+
+@register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('author', 'blurb', 'created',)
+    ordering = ('-created', 'blurb', 'author',)
--- comment/models.py
+++ comment/models.py
@@ -6,7 +6,7 @@ from blurb.models import Blurb
 # Create your models here.
 class Comment(models.Model):
     blurb = models.ForeignKey(Blurb, on_delete=models.CASCADE, related_name='comments')
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
     body = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
@@ -15,4 +15,4 @@ class Comment(models.Model):
         ordering = ('-created',)
 
     def __str__(self):
-        return 'Comment by {} on {}'.format(self.user, self.blurb)
+        return 'Comment by {} on {}'.format(self.author, self.blurb)
